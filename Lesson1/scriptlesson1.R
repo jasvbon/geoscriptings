6336f2f78957e154a0371b7f8f020cc2ceed8152
jasvbon <jasper.vanbon@wur.nl>
2020-01-06 12:01:45 +0100

    End Session 1

---
+++ Lesson1/scriptlesson1.R
@@ -0,0 +1,12 @@
+library(raster)
+r <- s <- raster(ncol=50, nrow=50)
+## Fill the raster with values
+r[] <- 1:ncell(r)
+s[] <- 2 * (1:ncell(s))
+s[200:400] <- 150
+s[50:150] <- 151
+## Perform the replacement
+r[s %in% c(150, 151)] <- NA
+## Visualise the result
+plot(r)
+
